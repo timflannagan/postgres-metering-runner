1bff8b7032a3c9a0415d693355dee0c01e12a83b
timflannagan1 <timflannagan@gmail.com>
2020-10-12 12:48:54 -0400

    hack: Fix misspelt base manifest directory

--- hack/install.sh
+++ hack/install.sh
@@ -11,7 +11,7 @@ fi
 oc -n ${NAMESPACE} adm policy add-scc-to-user -z default anyuid
 
 if ! oc -n openshift-monitoring get prometheusrules metering >/dev/null 2>&1; then
-    oc create -f manifezsts/monitoring/recording-rules.yaml
+    oc create -f manifests/monitoring/recording-rules.yaml
 fi
 
 if ! oc -n ${NAMESPACE} get deployment postgres 2> /dev/null; then
