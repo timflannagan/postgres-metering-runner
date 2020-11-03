22d48cdc0c6dededf43c930d4cd0ae273a1c1787
timflannagan1 <timflannagan@gmail.com>
2020-11-03 13:31:42 -0500

    hack: Update the Prometheus bash scripts

--- hack/get-metering-k8s-rules.sh
+++ hack/get-metering-k8s-rules.sh
@@ -5,4 +5,4 @@ set -eou pipefail
 token=$(oc -n openshift-monitoring sa get-token prometheus-k8s)
 host=$(oc -n openshift-monitoring get routes prometheus-k8s -o jsonpath={.spec.host})
 
-curl -k -s -H "Authorization: Bearer $token" https://${host}/api/v1/rules | faq -f json '.data.groups[] | select(.name | contains("metering.rules"))' | faq -f json '.rules[].name'
+curl -k -s -H "Authorization: Bearer $token" https://${host}/api/v1/rules | faq -f json '.data.groups[] | select(.name | contains("metering"))' | faq -f json '.rules[].name'
